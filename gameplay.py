473992d2ca9bdf375a64f4ea6aa0188f1cb27913
SoGoesLight <ilyalpt@gmail.com>
2020-09-22 18:13:49 +0300

    optimization, fixes

--- gameplay.py
+++ gameplay.py
@@ -116,14 +116,14 @@ def buy():
     k.press_button('b')
     m.move_to(settings.safe_point[0], settings.safe_point[1], 0.1)
 
-    pistols = enumerate(["shorty", "usp", "deagle"])
-    guns = enumerate(["phantom", "vandal", "awp"])
+    pistols = ["shorty", "usp", "deagle"]
+    guns = ["phantom", "vandal", "awp"]
 
     pistol = random.randint(0, 2)
     gun = random.randint(0, 2)
 
-    buy_gun(pistols[pistol], .75)
-    buy_gun(guns[gun], .75)
+    buy_gun(pistols[pistol], .85)
+    buy_gun(guns[gun], .85)
 
     time.sleep(0.2)
     k.press_button('b')
@@ -134,7 +134,7 @@ def buy_gun(gun, conf):
         x, y = pyautogui.locateCenterOnScreen('resources/' + settings.resolution_string
                                               + '/guns/' + gun + '.png', confidence=conf)
         m.click_on_center(x, y)
-        print("Bought ", gun, conf)
+        print("Bought", gun, conf)
     except TypeError:
         try:
             conf -= .05
