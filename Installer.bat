ab5b69aa10d0c54ba693ba81bb47f2a826a44607
SoGoesLight <ilyalpt@gmail.com>
2020-11-17 20:11:46 +0200

    Fixes

--- Installer.bat
+++ Installer.bat
@@ -1,7 +1,7 @@
 @echo off
 py -m pip install --upgrade pip
+py -m pip install numpy==1.19.3
 py -m pip install pyautogui==0.9.50
 py -m pip install colorthief==0.2.1
 py -m pip install opencv-python~=4.4.0.42
 py -m pip install colorama==0.4.4
-py -m pip install -U numpy==1.19.4
